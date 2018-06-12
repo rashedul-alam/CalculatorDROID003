c08a165e4e030481f89d5e891d77d402e3557272
rashedul-alam <26365486+rashedul-alam@users.noreply.github.com>
2018-06-12 14:21:00 +0600

    Add files via upload

--- Calculator/gen/calculator/red/R.java
+++ Calculator/gen/calculator/red/R.java
@@ -13,6 +13,35 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int Button01=0x7f050010;
+        public static final int Button02=0x7f050011;
+        public static final int Button03=0x7f050012;
+        public static final int Button04=0x7f05000b;
+        public static final int Button08=0x7f050016;
+        public static final int Button11=0x7f050017;
+        public static final int Button12=0x7f050015;
+        public static final int Button14=0x7f050018;
+        public static final int ButtonAC=0x7f05000f;
+        public static final int ButtonAdd=0x7f050003;
+        public static final int ButtonC=0x7f050014;
+        public static final int ButtonDiv=0x7f050009;
+        public static final int ButtonHR=0x7f05000a;
+        public static final int ButtonMC=0x7f050001;
+        public static final int ButtonMR=0x7f050002;
+        public static final int ButtonMS=0x7f050005;
+        public static final int ButtonMSub=0x7f050004;
+        public static final int ButtonMul=0x7f05000e;
+        public static final int ButtonPer=0x7f050019;
+        public static final int ButtonSub=0x7f050013;
+        public static final int TextView1=0x7f05001a;
+        public static final int button05=0x7f05000c;
+        public static final int button06=0x7f05000d;
+        public static final int button07=0x7f050006;
+        public static final int button08=0x7f050007;
+        public static final int button09=0x7f050008;
+        public static final int editText1=0x7f050000;
+    }
     public static final class layout {
         public static final int lanbd=0x7f030000;
         public static final int main=0x7f030001;
--- Calculator/src/calculator/red/CalculatorActivity.java
+++ Calculator/src/calculator/red/CalculatorActivity.java
@@ -1,13 +1,79 @@
 package calculator.red;
 
+import android.R.string;
 import android.app.Activity;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class CalculatorActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	private Button btn1;
+	private TextView tv1;
+
+	/** Called when the activity is first created. */
+	@Override
+   
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        if(this.getResources().getConfiguration() .orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+        	 setContentView(R.layout.main);
+        }
+        else if(this.getResources().getConfiguration() .orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+        	setContentView(R.layout.lanbd);
+        }
+        btn1 = (Button)findViewById(R.id.Button01);
+        tv1 =  (TextView)findViewById(R.id.TextView1);
+        
     }
+
+	public void oneClicked(View v) {
+		String t = tv1.getText().toString() + btn1.getText().toString();
+		tv1.setText(t);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+	
+	
+
 }
